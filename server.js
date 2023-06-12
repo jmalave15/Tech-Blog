84880da5c07dee82f68ddecbb2c2dc4f5a751209
jmalave15 <qmalavejose1@gmail.com>
2023-06-11 23:17:07 -0400

    server content and some models

---
+++ server.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const server = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ headers});
+
+const sess = {
+    secret: 'Super secret',
+    cookie: {
+        maxAge: 3600,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+server.use(session(sess));
+
+server.engine('handlebars', hbs.engine);
+server.set('view engine', 'handlebars');
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    server.listen(PORT, () => console.log('Now listening'));
+});
