874e336b6d67c44458408cda72b097a2eba58bfe
jmalave15 <qmalavejose1@gmail.com>
2023-06-21 16:05:14 -0400

    added handlebars and polished some js

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const server = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ headers});
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers,
+    defaultLayout: "main",
+});
 
 const sess = {
-    secret: 'Super secret',
+    secret: "Super secret",
     cookie: {
         maxAge: 300000,
         httpOnly: true,
         secure: false,
-        sameSite: 'strict',
+        sameSite: "strict",
     },
     resave: false,
     saveUninitialized: true,
@@ -29,15 +32,15 @@ const sess = {
 
 server.use(session(sess));
 
-server.engine('handlebars', hbs.engine);
-server.set('view engine', 'handlebars');
+server.engine("handlebars", hbs.engine);
+server.set("view engine", "handlebars");
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, "public")));
 
 server.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    server.listen(PORT, () => console.log('Now listening'));
+    server.listen(PORT, () => console.log("Now listening"));
 });
